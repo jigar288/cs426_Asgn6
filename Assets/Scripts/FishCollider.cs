4c0d42ab0416c7e4e08c97ec4fab0684486a5039
Dalasu2 <Dalasu2@uic.edu>
2021-04-02 12:16:58 -0500

    Added polar bear animations

--- Assets/Scripts/FishCollider.cs
+++ Assets/Scripts/FishCollider.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,26 +18,4 @@ public class FishCollider : MonoBehaviour {
             Destroy(this.gameObject);
         }
     }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FishCollider : MonoBehaviour {
-    FishCountController fcc;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        fcc = (GameObject.FindGameObjectWithTag("Canvas")).GetComponent<FishCountController>();
-    }
-
-    void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("PlayerObj")) {
-            fcc.updateImages(fcc.getFishCount());
-            fcc.updateFishCount(1);
-            Destroy(this.gameObject);
-        }
-    }
->>>>>>> f4d4002b9f9b6fd8977836e79fa09ca2f883209c
 }
